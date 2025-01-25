091eee982bc0bc2ea3ca54ebc74cae0d720561b1
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2025-01-25 09:40:43 -0300

    feat: atualização do seed da base de dados com relações

--- src/main/java/com/projetos/gamelist/entities/Belonging.java
+++ src/main/java/com/projetos/gamelist/entities/Belonging.java
@@ -1,6 +1,50 @@
 package com.projetos.gamelist.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
 public class Belonging {
-    private BelongingPK id;
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
     private Integer position;
+
+    public Belonging() {}
+
+    public Belonging(BelongingPK id, Integer position) {
+        this.id = id;
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Belonging belonging)) return false;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
